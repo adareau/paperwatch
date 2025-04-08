78da8af7901a99693864c8a9db2753280fb54256
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-04-08 17:48:15 +0200

    all papers page

--- www/papers.all.sql
+++ www/papers.all.sql
@@ -6,7 +6,7 @@ SELECT
 
 --- ================= BODY ====================
 
-select 'title' as component, 'papers : all' as contents;
+select 'title' as component, 'All papers' as contents;
 
 select
     'table' as component,
@@ -15,11 +15,9 @@ select
     'link' as markdown,
     True as sort;
 select
-    id,
     title,
-    author_display as "authors",
-    new,
-    analyzed,
-    selected,
-    format("%s", link) as "link"
-from papers;
+    substring(author_display, 1, 100) as "authors",
+    format("[%s](%s)",feed_display_name, link) as "link",
+    substring(date_added, 1, 10) as "date"
+
+from papers order by date_added desc limit 100;
